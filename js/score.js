86114d512cde623860f2fb10ed3ac3c8d9e1b44b
moss <strxzzs@gmail.com>
2026-03-25 10:05:52 +0300

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,30 +1,13 @@
-/**
- * Numbers of decimal digits to round to
- */
-const scale = 3;
-
-/**
- * Calculate the score awarded when having a certain percentage on a list level
- * @param {Number} rank Position on the list
- * @param {Number} percent Percentage of completion
- * @param {Number} minPercent Minimum percentage required
- * @returns {Number}
- */
 export function score(rank, percent, minPercent) {
-    if (rank > 150) {
-        return 0;
-    }
-    if (rank > 75 && percent < 100) {
+    // Cap at 50 ranks
+    if (rank > 50) {
         return 0;
     }
 
-    // Old formula
-    /*
-    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-    */
-    // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+    // curve (fits exactly rank 1 to 50)
+    let baseScore = 1 + 99 * Math.pow(1 - (rank - 1) / 49, 2.1);
+
+    let score = baseScore *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
@@ -34,21 +17,4 @@ export function score(rank, percent, minPercent) {
     }
 
     return Math.max(round(score), 0);
-}
-
-export function round(num) {
-    if (!('' + num).includes('e')) {
-        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
-    } else {
-        var arr = ('' + num).split('e');
-        var sig = '';
-        if (+arr[1] + scale > 0) {
-            sig = '+';
-        }
-        return +(
-            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
-            'e-' +
-            scale
-        );
-    }
 }
